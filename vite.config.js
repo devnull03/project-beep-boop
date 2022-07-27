db50ae269e5a3ccf07fb23bf999b9184ae7343a6
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-07-26 17:29:10 -0700

    feat: trying to make pwa work ༼ つ ◕_◕ ༽つ

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	base: '/project-beep-boop',
+  plugins: [sveltekit(), VitePWA({ registerType: "autoUpdate", scope: "/project-beep-boop" })],
+  base: "/project-beep-boop",
 };
 
 export default config;
